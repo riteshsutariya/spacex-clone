1ef174140a7f56b92ee1083f571c2e8da832e522
riteshsutariya <riteshsutariya94@gmail.com>
2022-12-30 14:54:59 +0530

    index page design completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
+const mobileMenu=document.getElementById('mobile-menu');
 
 btn.addEventListener("click", navToggle);
 
 function navToggle() {
   btn.classList.toggle("open");
   overlay.classList.toggle("overlay-show");
+  mobileMenu.classList.toggle('show-menu');
   document.body.classList.toggle("stop-scrolling");
 }
